fbb9268bb149f2acf28f823b7a688bbecb59116c
Juan luis Estrada <95386670+Novaversocl@users.noreply.github.com>
2023-12-10 12:38:40 -0300

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,21 +14,18 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+ 
     </p>
   </div>
 
   <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >*****</a
-    >Sitio en Mantención.
+  
+    Sitio en Mantención.
   </p>
   <p>
-    Install
+ 
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
+   
   </p>
   <p class="read-the-docs">Desarrollo Web Full Stack 2023. </p>
 </template>
