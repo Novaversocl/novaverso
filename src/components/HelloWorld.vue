285896f5450dceb2e613f37ae346dfba6b61fd6e
Otakid <juanestrada.n@gmail.com>
2023-12-16 17:45:35 -0300

    test visual

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,7 +27,7 @@ const count = ref(0)
    
    
   </p>
-  <p class="read-the-docs">Desarrollo Web Full Stack 2023. </p>
+  <p class="read-the-docs">Proyecto Web Full Stack 2023. </p>
 </template>
 
 <style scoped>
