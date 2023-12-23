269c12ba5925969c8ef24da8e2b77a79b8dec2f0
Otakid <juanestrada.n@gmail.com>
2023-12-22 22:39:53 -0300

    actualizar dependencias2

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+import { createApp } from 'vue';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import App from './App.vue';
+
